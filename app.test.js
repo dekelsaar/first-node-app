4b3fc76500472b86ee3f92a071f95d2721ecbd0f
dekelsaar <98514603+dekelsaar@users.noreply.github.com>
2022-07-10 03:21:14 +0300

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,21 +1,21 @@
 const request = require("supertest");
 const app = require("./app");
 
-describe("Test the root path", () => {
+describe("Test the / path", () => {
     test("It should response the GET method", () => {
       return request(app)
         .get("/")
         .expect(200);    
     });
   });
-describe("Test the root path", () => {
+describe("Test the /api/users path", () => {
     test("It should response the GET method", () => {
       return request(app)
         .get("/api/users")
         .expect(200);    
     });
   });
-describe("Test the root path", () => {
+describe("Test the /api/companies path", () => {
     test("It should response the GET method", () => {
       return request(app)
         .get("/api/companies")
